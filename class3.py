57dc97ae674ceb0aec4449b42f07d13e20ab905e
maalrmaih <118623708+maalrmaih@users.noreply.github.com>
2022-11-30 20:08:45 +0300

    done

--- class3.py
+++ class3.py
@@ -1,14 +1,17 @@
 # write your code here
-favorite_animals=['dog','cat','monkey','rabbit']
+favorite_animals = ['dog', 'cat', 'monkey', 'rabbit']
 print(favorite_animals)
-favorite_animals.pop(2)
+
 favorite_animals.remove('monkey')
+favorite_animals.pop(2)
 favorite_animals.append('hores')
+print(favorite_animals)
+
 for x in favorite_animals:
     print(f"love {x}")
 
 numbers=[1,2,3,4,5]    
 numbers_sum=0
-for nubmers in numbers:
-    numbers_sum+=numbers
+for number in numbers:
+    numbers_sum += number
 print(numbers_sum)
